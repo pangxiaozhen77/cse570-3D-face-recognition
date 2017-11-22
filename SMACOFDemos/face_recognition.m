30aa56832d0e3339057723c44a7ff0542adfaeda
tsztheseason <tszchan94@gmail.com>
2017-11-22 00:44:45 -0700

    Lay out program structure and add TODOs

--- SMACOFDemos/face_recognition.m
+++ SMACOFDemos/face_recognition.m
@@ -1,6 +1,41 @@
-% Load data
-dataDirectory = '../FaceData';
-files = dir(fullfile(dataDirectory, '*.mat'));
+% Load the gallery. Each surface in the gallery is sampled with
+% 1000 points.
+directory = '../FaceData';
+files = dir(fullfile(directory, '*.mat'));
 for i = 1:length(files)
-    faceDataSet(i) = load(fullfile(dataDirectory, files(i).name));
+    data = load(fullfile(directory, files(i).name));
+    data = remesh(data.human_face, set_options('vertices', 1000));
+    data = init_surface(data);
+    gallery(i) = data;
 end
+
+% TODO: Compute distortions between each surface in the gallery
+% using GMDS and quantize the distortions. Distortions that are
+% similar are quantize to a set that represents a class of surfaces.
+% To build the set, for each new surface, create a class and add
+% all surfaces with distortions that are within a threshold to the
+% set. Need to figure out how to determine the threshold. Also,
+% move this code to a function.
+gmdsSampleSize = 50;
+
+% Load the probes. Each probe is sampled with 1000 points.
+directory = '../4KMeshWFeaturePoints/4KMesh';
+files = dir(fullfile(directory, '*.mat'));
+for i = 1:length(files)
+    data = load(fullfile(directory, files(i).name));
+    data = remesh(data.human_face_4k, set_options('vertices', 1000));
+    data = init_surface(data);
+    probes(i) = data;
+end
+
+% TODO: For each probe, compute the distortion between the probe
+% and each surface in the gallery and find the surface with the
+% lowest distortion. Identify the class that the surface belongs
+% and compute the distortions between the probe and each surface
+% in the identified class. If the distortions between the probe
+% and the other surfaces in the class are within the threshold,
+% add the probe to the class. Need to figure out how to determine
+% the threshold, and whether all the distortions between the probe
+% and other surfaces in the class need to be within a threshold for
+% the probe to be identified as the class. Also, move this code to
+% a function.
---
+++ SMACOFDemos/face_recognition_algorithm.m
@@ -0,0 +1,10 @@
+% face_recognition_algorithm
+%
+% Description:
+% 
+% Inputs:
+%
+% Outputs:
+% 
+function [out] = face_recognition_algorithm(in)
+end
