4be6d0cf8ce27b55c0fe93f79a8d5864e71df179
tsztheseason <tszchan94@gmail.com>
2017-11-28 07:10:46 -0700

    Update code to use different training and testing set, and update TODOs

--- SMACOFDemos/face_recognition.m
+++ SMACOFDemos/face_recognition.m
@@ -1,23 +1,25 @@
 % Load the gallery. Each surface in the gallery is sampled with
-% 1000 points.
-directory = '../4KMeshWFeaturePoints/4KMesh';
+% 1000 number points.
+directory = '../FaceData';
 files = dir(fullfile(directory, '*.mat'));
+classRegex = 'human_face\d+';
 for i = 1:length(files)
     data = load(fullfile(directory, files(i).name));
-    data = remesh(data.human_face_4k, set_options('vertices', 1000));
+    data = remesh(data.human_face, set_options('vertices', 1000));
     data = init_surface(data);
     gallery(i) = data;
+    classes{i} = regexp(files(i).name, classRegex, 'match', 'once');
 end
 
-% TODO: Compute distortions between each surface in the gallery
-% using GMDS and quantize the distortions. Distortions that are
-% similar are quantize to a set that represents a class of surfaces.
-% To build the set, for each new surface, create a class and add
-% all surfaces with distortions that are within a threshold to the
-% set. Need to figure out how to determine the threshold. Also,
-% move this code to a function.
-gmdsSampleSize = 50;
-classDistortionThreshold = 1;
+% TODO: To build the database, compute distortions between each
+% surfaces in the gallery using GMDS and group together surfaces
+% with similar distortions. Surfaces with similar distortions
+% form a set that represents a class of surfaces.
+%
+% Questions:
+%   - Are the identity of the surfaces known in advance?
+%   - What is an appropriate threshold between distortion values
+%     to form a group?
 
 % Load the probes. Each probe is sampled with 1000 points.
 directory = '../4KMeshWFeaturePoints/4KMesh';
@@ -29,14 +31,10 @@ for i = 1:length(files)
     probes(i) = data;
 end
 
-% TODO: For each probe, compute the distortion between the probe
-% and each surface in the gallery and find the surface with the
-% lowest distortion. Identify the class that the surface belongs
-% and compute the distortions between the probe and each surface
-% in the identified class. If the distortions between the probe
-% and the other surfaces in the class are within the threshold,
-% add the probe to the class. Need to figure out how to determine
-% the threshold, and whether all the distortions between the probe
-% and other surfaces in the class need to be within a threshold for
-% the probe to be identified as the class. Also, move this code to
-% a function.
+% TODO: To test a probe, compute the distortion between the probe
+% and each surfaces in the gallery. Find the surface with the lowest
+% distortion within a threshold. If a surface is found, the class of
+% the found surface is the class of the probe.
+%
+% Questions:
+%   - What threshold should be used?
