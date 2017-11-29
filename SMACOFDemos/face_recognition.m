428ce8bf6e68450db7630b09b17ae44c1f49ff70
meerabenson <meeratucson@yahoo.com>
2017-11-28 21:12:04 -0700

    Update face_recognition

--- SMACOFDemos/face_recognition.m
+++ SMACOFDemos/face_recognition.m
@@ -1,40 +1,33 @@
-% Load the gallery. Each surface in the gallery is sampled with
-% 1000 number points.
+% Load the probe. It is sampled with 1000 points.
+j = 1; %index of the probe to be used
 directory = '../FaceData';
 files = dir(fullfile(directory, '*.mat'));
-classRegex = 'human_face\d+';
-for i = 1:length(files)
-    data = load(fullfile(directory, files(i).name));
-    data = remesh(data.human_face, set_options('vertices', 1000));
-    data = init_surface(data);
-    gallery(i) = data;
-    classes{i} = regexp(files(i).name, classRegex, 'match', 'once');
-end
+probe = load(fullfile(directory, files(j).name));
+probe = remesh(probe.human_face, set_options('vertices', 1000));
+probe = init_surface(probe);
 
-% TODO: To build the database, compute distortions between each
-% surfaces in the gallery using GMDS and group together surfaces
-% with similar distortions. Surfaces with similar distortions
-% form a set that represents a class of surfaces.
-%
-% Questions:
-%   - Are the identity of the surfaces known in advance?
-%   - What is an appropriate threshold between distortion values
-%     to form a group?
-
-% Load the probes. Each probe is sampled with 1000 points.
+% Load the gallery. Each surface in the gallery is sampled with
+% 1000 number points.
 directory = '../4KMeshWFeaturePoints/4KMesh';
 files = dir(fullfile(directory, '*.mat'));
+
+% To test a probe, compute the distortion between the probe
+% and each surfaces in the gallery. Find the surface with the lowest
+% distortion within a threshold. If a surface is found, the 
+% probe belongs to the class of the found surface.
+
 for i = 1:length(files)
     data = load(fullfile(directory, files(i).name));
     data = remesh(data.human_face_4k, set_options('vertices', 1000));
     data = init_surface(data);
-    probes(i) = data;
+    [tx, ux, ty, uy, f, rmsdist, maxdist, local_stress] = gmds (data, probe, N);
+    distances(i) = rmsdist;  
 end
 
-% TODO: To test a probe, compute the distortion between the probe
-% and each surfaces in the gallery. Find the surface with the lowest
-% distortion within a threshold. If a surface is found, the class of
-% the found surface is the class of the probe.
-%
+% Print out the distances between the probe and surfaces in the gallery
+for i = 1:length(distances)
+    fprintf(1, 'RMD distortion between probe %d and %d  = %8.6f\n', j, i, distances(i));   
+end
+
 % Questions:
 %   - What threshold should be used?
